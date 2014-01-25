e37a63ff14a882ec52613e754ee9338ad063b2fb
numb3r3 <wffrank1987@gmail.com>
2014-01-25 23:13:21 +0800

    add the integer sequence generator using closure

---
+++ gorec/utils/common.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2014 Feng Wang <wffrank1987@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language
+
+package utils
+
+import "fmt"
+
+// Return sequence integers with the begining value as x
+func IntSequence(begin int) func() int {
+    i := begin
+    return func() int {
+        i += 1
+        return i
+    }
+}
+
