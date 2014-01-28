aeb6801dd17ed39630c70b993ffdfa75665f86b3
numb3r3 <wffrank1987@gmail.com>
2014-01-28 13:33:12 +0800

    add map/reduce/filter supporting using reflect in go

--- gorec/utils/common.go
+++ gorec/utils/common.go
@@ -13,7 +13,10 @@
 
 package utils
 
-import "fmt"
+import (
+       "fmt"
+       "reflect"
+}
 
 // Return sequence integers with the begining value as x
 func IntSequence(x int) func() int {
@@ -24,3 +27,65 @@ func IntSequence(x int) func() int {
     }
 }
 
+// from: https://gist.github.com/rafkhan/6501567
+
+// map function types
+type mapf func(interface{}) interface{}
+
+// reduce function types
+type reducef func(interface{}, interface{}) interface{}
+
+
+// filter function types
+type filterf func(interface{}) bool
+
+// Map(slice, func)
+//
+// Usages:
+//     a := []int{1, 2, 3, 4}
+//     b := Map(a, func(val interface{}) interface{} {
+//     	 return val.(int) * 2
+//     })
+// should be [2, 4, 6, 8]
+func Map(in interface{}, fn mapf) interface {} {
+     val := reflect.ValueOf(in)
+     out := make([]interface{}, val.Len())
+     
+     for i := 0; i < val.Len(); i++ {
+     	 out[i] = fn(val.Index(i).Interface())
+     }
+     return out
+}
+
+//Reduce(slice, starting value, func)
+//
+//Usage:
+// c := Reduce(b, 0, func(val interface{}, memo interface{}) interface{} {
+//   return memo.(int) + val.(int)
+// })
+func Reduce(in interface{}, memo interface{}, fn reducef) interface{} {
+     val := reflect.ValueOf(in)
+     
+     for i:=0; i < val.Len(); i++ {
+     	 memo = fn(val.Index(i).Interface(), memo)
+     }
+     return memo
+}
+
+//Filter(slice, predicate func)
+//Usage:
+// d := Filter(b, func(val interface{}) bool {
+//   return val.(int) % 4 == 0
+//})
+func Filter(in interface{}, fn filterf) interface{} {
+     val := reflect.ValueOf(in)
+     out := make([]interface{}, 0, val.Len())
+
+     for i := 0; i < val.Len(); i++ {
+     	 current := val.Index(i).Interface()
+	 if fn(current) {
+	    out = append(out, current)
+	 }
+     }
+     return out
+}
