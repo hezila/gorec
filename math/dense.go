e5b3eaadfb7542be535ac29955611dd057f9f697
numb3r3 <wffrank1987@gmail.com>
2014-02-11 15:18:36 +0800

    borrow all of codes in dense.go from go.matrix

--- math/dense.go
+++ math/dense.go
@@ -14,8 +14,8 @@
 package math
 
 import (
-	"fmt"
 	"log"
+	"math/rand"
 )
 
 // A matrix backed by a flat array of all elements
@@ -106,8 +106,8 @@ func (M *DenseMatrix) Set(i, j int, v float64) {
 // Get a submatrix starting at i, j with rows rows and cols columns
 func (M *DenseMatrix) SubMatrix(i, j, rows, cols int) *DenseMatrix {
 	if (i + rows) > M.rows || (j + cols) > M.cols {
-		fmt.Printf("R: %d\t%d\n", i+rows, M.rows)
-		fmt.Printf("C: %d\t%d\n", j+cols, M.cols)
+		//fmt.Printf("R: %d\t%d\n", i+rows, M.rows)
+		//fmt.Printf("C: %d\t%d\n", j+cols, M.cols)
 		log.Fatal("index out of bounds")
 	}
 	A := new(DenseMatrix)
@@ -121,6 +121,15 @@ func (M *DenseMatrix) SubMatrix(i, j, rows, cols int) *DenseMatrix {
 		}
 	}
 	return A
+}
+
+// Copy A into M, with A's 0, 0 aligning with A's i, j
+func (M *DenseMatrix) SetMatrix(i, j int, A *DenseMatrix) {
+	for r := 0; r < A.rows; r++ {
+		for c := 0; c < A.cols; c++ {
+			M.Set(i + r, j + c, A.Get(r, c))
+		}
+	}
 }
 
 func (M *DenseMatrix) ColVector(j int) *DenseMatrix {
@@ -144,16 +153,129 @@ func (M *DenseMatrix) Copy() *DenseMatrix {
 	return A
 }
 
+
+// Get a new matrix [M, A]
+func (M *DenseMatrix) Augment(A *DenseMatrix) (B *DenseMatrix, err error) {
+	if M.rows != A.rows {
+		err = ErrorDimensionMismatch
+		return
+	}
+	B = Zeros(M.rows, M.cols + A.cols)
+	err = M.AugmentFill(A, B)
+	return
+}
+
+
+// Get a new matrix [M; A], with M above A
+func (M *DenseMatrix) Stack(A *DenseMatrix) (B *DenseMatrix, err error) {
+	if M.cols != A.cols {
+		err = ErrorDimensionMismatch
+		return
+	}
+	B = Zeros(M.rows + A.rows, M.cols)
+	err = M.StackFill(A, B)
+	return
+}
+
+func (M *DenseMatrix) StackFill(A, B *DenseMatrix) (err error) {
+	if M.cols != A.cols || M.cols != B.cols || B.rows != M.rows + A.rows {
+		err = ErrorDimensionMismatch
+		return
+	}
+	B.SetMatrix(0, 0, M)
+	B.SetMatrix(M.rows, 0, A)
+	return
+}
+
+
 func (M *DenseMatrix) AugmentFill(A, B *DenseMatrix) (err error) {
 	if M.rows != A.rows || M.rows != B.rows || B.cols != M.rows + A.rows {
 		err = ErrorDimensionMismatch
 		return
 	}
 	
-	// TODO
-	// B.SetMatrix(0, 0, M)
-	// B.SetMatrix(0, A.cols, A)
+
+	B.SetMatrix(0, 0, M)
+	B.SetMatrix(0, A.cols, A)
 	return
 }
 
+
+
 func (M *DenseMatrix) String() string { return String(M) }
+
+func Zeros(rows, cols int) *DenseMatrix {
+	Z := new(DenseMatrix)
+	Z.elements = make([]float64, rows * cols)
+	Z.rows = rows
+	Z.cols = cols
+	Z.step = cols
+	return Z
+}
+
+func Ones(rows, cols int) *DenseMatrix {
+	O := new(DenseMatrix)
+	O.elements = make([]float64, rows * cols)
+	O.rows = rows
+	O.cols = cols
+	O.step = cols
+	for i := 0; i < len(O.elements); i++ {
+		O.elements[i] = 1
+	}
+	return O
+}
+
+func Eye(size int) *DenseMatrix {
+	E := Zeros(size, size)
+	for i := 0; i < size; i++ {
+		E.Set(i, i, 1)
+	}
+	return E
+}
+
+func Normals(rows, cols int) *DenseMatrix {
+	N := Zeros(rows, cols)
+	for i := 0; i < N.Rows(); i++ {
+		for j := 0; j < N.Cols(); j++ {
+			N.Set(i, j, rand.NormFloat64())
+		}
+	}
+	return N
+}
+
+func Diagonal(d []float64) *DenseMatrix {
+	n := len(d)
+	D := Zeros(n, n)
+	for i := 0; i < n; i++ {
+		D.Set(i, i, d[i])
+	}
+	return D
+}
+
+func MakeDenseCopy(M Matrix) *DenseMatrix {
+	A := Zeros(M.Rows(), M.Cols())
+	for i := 0; i < M.Rows(); i++ {
+		for j := 0; j < M.Cols(); j++ {
+			A.Set(i, j, M.Get(i, j))
+		}
+	}
+	return A
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
